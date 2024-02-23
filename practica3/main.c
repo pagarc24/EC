7b31e71c29edaa988b77c6aeea99247ee6118129
Pablo <79839698+pagarc24@users.noreply.github.com>
2024-02-23 13:36:46 +0100

    Reestructuración ejercicio3
    Nueva logística y finalización del ejercicio2

--- practica3/main.c
+++ practica3/main.c
@@ -2,65 +2,68 @@
 #include <msp430.h>
 #include <msp430fr6989.h>
 
-//INTERRUPCIONES
-//Comentarlas si fuera necesario
+/*
+    EJERCICIO 1
 
-//ej2
-// Port 1 ISR
-# pragma vector = PORT1_VECTOR
-__interrupt void Port_1 ( void ) {
+    // Mientras el botón P1.1 se mantiene pulsado el led P1.0 está encendido. En caso contrario se queda apagado
+    void main(void){
+        WDTCTL = WDTPW | WDTHOLD;
+        PM5CTL0 &= ~LOCKLPM5;
 
-    if(P1IN & BIT1){//los pushbuttons funcionan en lógica inversa
-        P1OUT &= ~BIT0;
-    } else {
-        P1OUT |= BIT0;
-    }
+        P1DIR = BIT0;
+        P1REN = BIT1;
+        P1OUT = BIT1;
 
-    P1IFG &= ~BIT1 ; // P1 .1 clear pending interrupts
-}
-
-//FUNCIONES
-void ej1(void){
-    WDTCTL = WDTPW | WDTHOLD;
-    PM5CTL0 &= ~LOCKLPM5;
-
-    P1DIR = BIT0;
-    P1REN = BIT1;
-    P1OUT = BIT1;
-
-    while(1){
-        if(P1IN & BIT1){//los pushbuttons funcionan en lógica inversa
-            P1OUT &= ~BIT0;
-        } else {
-            P1OUT |= BIT0;
+        while(1){
+            if(P1IN & BIT1){//los pushbuttons funcionan en lógica inversa
+                P1OUT &= ~BIT0;
+            } else {
+                P1OUT |= BIT0;
+            }
         }
     }
-}
-void ej2(void){
-    // disable watchdog
-    WDTCTL = WDTPW | WDTHOLD;
-    // disable high impeadance on I / O
-    PM5CTL0 &= ~LOCKLPM5;
 
-    // set P1 .0 as output , rest as inputs
-    P1DIR = BIT0;
-    // enable pull on P1 .1 , rest disabled
-    P1REN = BIT1;
-    // set pull - up on P1 .1
-    P1OUT = BIT1;
+*/
 
-    // P1 .1 IRQ enabled
-    P1IE |= BIT1;
+/*
+    EJERCICIO 2
 
-    // P1 .1 Falling edge
-    P1IES |= BIT1;
-    // P1 .1 clear pending interrupts
-    P1IFG &= ~ BIT1 ;
+    // Cada vez que pulsemos el botón P1.1 el led P1.0 cambia de estado (de encendido a apagado y viceversa)
+    # pragma vector = PORT1_VECTOR
+    __interrupt void Port_1 ( void ) {
+        P1OUT ^= BIT0; // Switch Bit1 value
+        P1IFG &= ~BIT1 ; // P1 .1 clear pending interrupts
+    }
 
-    __bis_SR_register ( LPM4_bits + GIE ) ; // Enter LMP4 mode ( sleep )
-}
+    void main(void){
+        // disable watchdog
+        WDTCTL = WDTPW | WDTHOLD;
+        // disable high impeadance on I / O
+        PM5CTL0 &= ~LOCKLPM5;
 
-void main(void){
-    //ej1();
-    ej2();
-}
+        // set P1 .0 as output , rest as inputs
+        P1DIR = BIT0;
+        // enable pull on P1 .1 , rest disabled
+        P1REN = BIT1;
+        // set pull - up on P1 .1
+        P1OUT = BIT1;
+
+        // P1 .1 IRQ enabled
+        P1IE |= BIT1;
+
+        // P1 .1 Falling edge
+        P1IES |= BIT1;
+        // P1 .1 clear pending interrupts
+        P1IFG &= ~ BIT1 ;
+
+        __bis_SR_register ( LPM4_bits + GIE ) ; // Enter LMP4 mode ( sleep )
+    }
+
+*/
+
+/*
+    EJERCICIO 3
+
+ */
+
+//
